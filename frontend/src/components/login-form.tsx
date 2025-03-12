674a4a580ef50457856a3d7ed852e32bf985c314
joshyt100 <joshyt100@gmail.com>
2025-03-11 22:00:55 -0400

    changed sign up

--- frontend/src/components/login-form.tsx
+++ frontend/src/components/login-form.tsx
@@ -110,7 +110,7 @@ export function LoginForm({
             </div>
             <div className="mt-4 text-center text-sm">
               Don&apos;t have an account?{" "}
-              <a href="/sign-up" className="underline underline-offset-4">
+              <a onClick={() => navigate("/signup")} className="underline underline-offset-4">
                 Sign up
               </a>
             </div>
