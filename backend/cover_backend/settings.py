ab53b2c9a00994057935244e9e27ee6caf0c44d4
joshyt100 <joshyt100@gmail.com>
2025-03-11 23:51:02 -0400

    changed settings

--- backend/cover_backend/settings.py
+++ backend/cover_backend/settings.py
@@ -218,12 +218,13 @@ STORAGES = {
 }
 
 AUTH_USER_MODEL = "accounts.CustomUser"
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5174",
-    "http://127.0.0.1:5174",
-    "https://ai-cover-letter-generator-i2xa.onrender.com",
-    "https://ai-cover-letter-generator-mrxj.vercel.app",
-]
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:5174",
+#     "http://127.0.0.1:5174",
+#     "https://ai-cover-letter-generator-i2xa.onrender.com",
+#     "https://ai-cover-letter-generator-mrxj.vercel.app",
+# ]
 CORS_ALLOW_CREDENTIALS = True
 # ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 CSRF_TRUSTED_ORIGINS = [
